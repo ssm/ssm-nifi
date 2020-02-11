056ee5a343cf052eea7404a4775416ccbd2c372d
Stig Sandbeck Mathisen <ssm@fnord.no>
2020-02-11 07:09:09 +0100

    Test for properties being set

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -12,10 +12,27 @@ describe 'nifi::config' do
           nifi_properties: {},
           user: 'nifi',
           group: 'nifi',
+          nifi_properties: {
+            'test.foo.bar': 'thud grunt'
+          }
         }
       end
 
       it { is_expected.to compile }
+
+      # Implicit property from var_directory
+      it { is_expected.to contain_ini_setting('nifi property nifi.flowfile.repository.directory')
+                            .with_path('/opt/nifi/nifi-1.2.3/conf/nifi.properties')
+                            .with_setting('nifi.flowfile.repository.directory')
+                            .with_value('/var/opt/nifi/flowfile_repository')
+      }
+
+      # Explicit property
+      it { is_expected.to contain_ini_setting('nifi property test.foo.bar')
+                            .with_path('/opt/nifi/nifi-1.2.3/conf/nifi.properties')
+                            .with_setting('test.foo.bar')
+                            .with_value('thud grunt')
+      }
     end
   end
 end

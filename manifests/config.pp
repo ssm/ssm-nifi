415c8843d0326aa200656a002043d157623e2e97
Stig Sandbeck Mathisen <ssm@fnord.no>
2020-02-11 07:19:29 +0100

    Manage nifi.properties

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,5 +3,43 @@
 # Private subclass for Apache NiFi configuration
 #
 # @api private
-class nifi::config {
+class nifi::config (
+  Stdlib::Absolutepath $install_root,
+  Stdlib::Absolutepath $var_directory,
+  Hash $nifi_properties,
+  String $user,
+  String $group,
+  String $version,
+) {
+
+  $software_directory = "${install_root}/nifi-${version}"
+  $nifi_properties_file = "${software_directory}/conf/nifi.properties"
+
+  $path_properties = {
+    'nifi.nar.working.directory'                   => "${var_directory}/work/nar/",
+    'nifi.documentation.working.directory'         => "${var_directory}/work/docs/components",
+    'nifi.database.directory'                      => "${var_directory}/database_repository",
+    'nifi.flowfile.repository.directory'           => "${var_directory}/flowfile_repository",
+    'nifi.content.repository.directory.default'    => "${var_directory}/content_repository",
+    'nifi.provenance.repository.directory.default' => "${var_directory}/provenance_repository",
+    'nifi.web.jetty.working.directory'             => "${var_directory}/work/jetty",
+  }
+
+  $_nifi_properties = $path_properties + $nifi_properties
+
+  file { $var_directory:
+    ensure => directory,
+    owner  => $user,
+    group  => $group,
+    mode   => '0750',
+  }
+
+  $_nifi_properties.each |String $key, String $value| {
+    ini_setting { "nifi property ${key}":
+      ensure  => present,
+      path    => $nifi_properties_file,
+      setting => $key,
+      value   => $value,
+    }
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -37,6 +37,14 @@
 # @param install_root
 #   The root directory of the nifi installation.
 #
+# @param var_directory
+#   The root of the writable paths used by NiFi. Nifi will create
+#   directories beneath this path.  This will implicitly add nifi
+#   properties for working directories and repositories.
+#
+# @param nifi_properties
+#   Hash of parameter key/values to be added to conf/nifi.properties.
+#
 # @example Defaults
 #   include nifi
 #
@@ -57,7 +65,9 @@ class nifi (
   String $download_checksum_type = 'sha256',
   Integer $service_limit_nofile = 50000,
   Integer $service_limit_nproc = 10000,
+  Hash $nifi_properties = {},
   Stdlib::Absolutepath $install_root = '/opt/nifi',
+  Stdlib::Absolutepath $var_directory = '/var/opt/nifi',
 ) {
 
   class { 'nifi::install':
@@ -71,7 +81,14 @@ class nifi (
     download_tmp_dir       => $download_tmp_dir,
   }
 
-  include nifi::config
+  class { 'nifi::config':
+    install_root    => $install_root,
+    user            => $user,
+    group           => $group,
+    var_directory   => $var_directory,
+    nifi_properties => $nifi_properties,
+    version         => $version,
+  }
 
   class {'nifi::service':
     install_root => $install_root,
